8a0509dabb2f37ece25e2624d33c17c1faedd732
Harun GÜLGÜN <harungulgun@gmail.com>
2017-01-08 01:07:09 +0300

    Kullanıcı Sayfasının bootstrapli html tasarımı yapıldı

---
+++ 156901025 interner-prog/index.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html>
+	<head>
+		<title>Personel İletişim Bilgisi</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="keywords" content="telefon,defteri,uygulaması" />
+		<meta name="description" content="Telefon numaralarının saklanacağı bir yazılım" />
+		<link rel="stylesheet" type="text/css" href="css/genel.css" charset="UTF-8" media="all" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> 
+		<script type="text/javascript" src="js/genel.js"></script>
+	</head>
+	<body>
+		<div class="genelDiv">
+		<h2 class="baslik">Personel İletişim Bilgisi</h2>
+		<form>
+		  <label for="kMail">Email</label>
+			<input type="text" name="kMail" id="kMail">
+			<label for="kSifre">Şifre</label>
+			<input type="password" name="kSifre" id="kSifre">
+			<button type="button" id="telGirisi">Giriş Yap</button>
+		</form>
+		<div class="cevap"></div>
+	</div>
+	</body>
+</html>
