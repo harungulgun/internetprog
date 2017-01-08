e8367be3c34957f5447a0d1bf1172336361717a0
Harun GÜLGÜN <harungulgun@gmail.com>
2017-01-08 14:33:17 +0300

    Kullanıcı girişi kontrol ediliyor .
    Kullanıcı girişi kontrol edilip eğer eksik alan varsa veya hatalı giriş
    yapıldıysa mesaj veriyor. Yoksa devam edecek.

---
+++ 156901025 interner-prog/site/girisKontrol.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+session_start();
+
+$kMail=$_POST["kMail"];
+$kSifre=$_POST["kSifre"];
+$mail="admin";
+$sifre="123";
+
+if (empty($kMail) || empty($kSifre)) {
+	
+	echo "Boş alanları doldurmak zorunludur!";
+} elseif (($kMail != $mail) || ($kSifre != $sifre)) {
+	
+	echo "Kullanıcı adı veya Parola hatalı girildi!";
+} else {
+	
+	$_SESSION['kadi'] = TRUE;
+	echo "tamam";
+}
